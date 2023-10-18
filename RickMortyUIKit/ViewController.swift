304e8255df94a872382847c7387221b5d7ec9575
cmuoz.altimetrik@ancestry.com <cmuoz.altimetrik@ancestry.com>
2023-10-18 16:21:13 -0500

    Initial Commit

---
+++ RickMortyUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMortyUIKit
+//
+//  Created by Carlos Mario Muñoz on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
